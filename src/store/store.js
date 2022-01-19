9f432dbe9814b229dc1ade2f5a492cb9d8653841
ZoharRo <zozozohar@gmail.com>
2022-01-19 11:26:17 +0200

    Zohar pushing first

--- src/store/store.js
+++ src/store/store.js
@@ -2,17 +2,11 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 
-import { toyReducer } from "./toy.reducer.js"
-// import { userReducer } from './user.reducer.js'
-// import { msgReducer } from "./msg.reducer.js"
-// import {reviewReducer} from './review.reducer.js'
+import { BoardReducer } from "./board.reducer.js"
 
 
 const rootReducer = combineReducers({
-    toyModule: toyReducer,
-    // userModule: userReducer,
-    // msgModule: msgReducer,
-    // reviewModule: reviewReducer
+    BoardModule: BoardReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
