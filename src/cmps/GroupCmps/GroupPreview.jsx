84d0c2f30782ddcaf3e639e528f4b2ece5a92297
Lee Segal <segalee17@gmail.com>
2022-01-19 12:53:45 +0200

    changed spelling in Gpreview

--- src/cmps/GroupCmps/GroupPreview.jsx
+++ src/cmps/GroupCmps/GroupPreview.jsx
@@ -1,10 +1,10 @@
-import {TaskList} from '../TaskCmps/TaskList.jsx'
-export function GroupPreview(){
-    return(
-   
-            <section>
-                <h1>group detiled</h1>
-                <TaskList/>
-            </section>
+import { TaskList } from '../TaskCmps/TaskList.jsx'
+export function GroupPreview() {
+    return (
+
+        <section>
+            <h1>group details</h1>
+            <TaskList />
+        </section>
     )
 }
