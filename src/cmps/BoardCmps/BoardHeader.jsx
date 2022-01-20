87204c6571e7c4d474b4737f0116c12db8d4e157
Lee Segal <segalee17@gmail.com>
2022-01-20 16:52:03 +0200

    started header

--- src/cmps/BoardCmps/BoardHeader.jsx
+++ src/cmps/BoardCmps/BoardHeader.jsx
@@ -1,9 +1,35 @@
-import {BoardControllers} from './BoardControllers.jsx'
-export function BoardHeader() {
+import { connect } from 'react-redux';
+import { BoardControllers } from './BoardControllers.jsx'
+export function _BoardHeader({ board }) {
+  console.log('board:', board);
+
   return (
     <section className="board-header">
-      <h1>header</h1>
-      <BoardControllers/>
+      {board && <div>
+        <h1 className='board-title' contentEditable
+          suppressContentEditableWarning={true}
+        >
+          {board.title}
+        </h1>
+        <p className="board-description"
+          contentEditable
+          suppressContentEditableWarning={true}
+        >{board.description}
+        </p>
+      </div>}
+      {/* <h1>{board.title}</h1> */}
+      <BoardControllers />
     </section>
   );
 }
+
+
+function mapStateToProps({ boardModule }) {
+  return {
+    board: boardModule.board,
+  };
+}
+const mapDispatchToProps = {
+}
+
+export const BoardHeader = connect(mapStateToProps, mapDispatchToProps)(_BoardHeader);
