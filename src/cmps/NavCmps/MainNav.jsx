32ed5389ed27fa4664a3ac92efa672e944f10056
Lee Segal <segalee17@gmail.com>
2022-01-22 19:58:44 +0200

    changed the main nav a bit

--- src/cmps/NavCmps/MainNav.jsx
+++ src/cmps/NavCmps/MainNav.jsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-import { HiSparkles, HiOutlinePuzzle } from "react-icons/hi";
-import { AiOutlineBell, AiOutlineSearch } from "react-icons/ai";
-import { BiGridAlt } from "react-icons/bi";
-import { BsCalendar2Check, BsInbox } from "react-icons/bs";
-import { RiUserAddLine, RiQuestionMark } from "react-icons/ri";
-import logo from "../../assets/imgs/logo.png";
-export function MainNav() {
-  return (
-    <section className="main-nav">
-      <img className="2day-logo" src={logo} alt=""></img>
-      <div className="grid-container">
-        <BiGridAlt
-          color="white"
-          size="23px"
-          style={{ marginLeft: "6px", marginTop: "6px", cursor: "pointer" }}
-        />
-      </div>
-      <AiOutlineBell
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <BsInbox
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <BsCalendar2Check
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <div className="see-plans flex align-center justify-center">
-        <h2>
-          <HiSparkles
-            color="white"
-            style={{
-              textAlign: "center",
-            }}
-          />
-          See Plans{" "}
-        </h2>
-      </div>
-      <HiOutlinePuzzle
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <RiUserAddLine
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <AiOutlineSearch
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <RiQuestionMark
-        color="white"
-        size="23px"
-        style={{ marginLeft: "19px", cursor: "pointer" }}
-      />
-      <div className="user-avatar">MZL</div>
-    </section>
-  );
-=======
 import { Link } from 'react-router-dom'
 import { HiSparkles, HiOutlinePuzzle } from 'react-icons/hi'
 import { AiOutlineBell, AiOutlineSearch } from 'react-icons/ai'
@@ -76,15 +8,17 @@ import logo from '../../assets/imgs/logo.png'
 export function MainNav() {
     return (
         <section className="main-nav">
-        <Link to='/'>
-            <img className="2day-logo" src={logo} alt=""></img>
-            </Link>
-           <div className='grid-container'>
-            <BiGridAlt color='white' size='23px' style={{marginLeft: "6px", marginTop: "6px", cursor: "pointer"}} />
+            <div className='top-opt flex'>
+                <Link to='/'>
+                    <img className="2day-logo" src={logo} alt=""></img>
+                </Link>
+                <div className='grid-container'>
+                    <BiGridAlt color='white' size='23px' style={{ marginLeft: "6px", marginTop: "6px", cursor: "pointer" }} />
+                </div>
+                <AiOutlineBell color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <BsInbox color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <BsCalendar2Check color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
             </div>
-            <AiOutlineBell color='white' size='23px'  style={{marginLeft: "19px", cursor: "pointer"}}  />
-            <BsInbox color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
-            <BsCalendar2Check color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
             <div className="see-plans flex align-center justify-center">
                 <h2><HiSparkles color='white'
                     style={{
@@ -92,15 +26,16 @@ export function MainNav() {
                     }} />
                     See Plans  </h2>
             </div>
-            <HiOutlinePuzzle color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
-            <RiUserAddLine color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
-            <AiOutlineSearch color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
-            <RiQuestionMark color='white' size='23px' style={{marginLeft: "19px", cursor: "pointer"}}  />
-<div className='user-avatar'>MZL</div>
+            <div className='bottom-opt flex'>
+                <HiOutlinePuzzle color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <RiUserAddLine color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <AiOutlineSearch color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <RiQuestionMark color='white' size='23px' style={{ marginLeft: "19px", cursor: "pointer" }} />
+                <div className='user-avatar'>MZL</div>
+            </div>
         </section>
     )
 
->>>>>>> 9ad8564def567f9730d26887a7fb76fa0bd15e72
 }
 
 // function mapStateToProps({ userModule }) {
