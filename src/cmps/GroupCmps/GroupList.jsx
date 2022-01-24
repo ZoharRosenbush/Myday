e793a0e11ff65e8bb59891deafd8b9a8875b4655
May Elgarat <mayelgarat1@gmail.com>
2022-01-24 21:13:27 +0200

    drag and drop works well!!!!!!

--- src/cmps/GroupCmps/GroupList.jsx
+++ src/cmps/GroupCmps/GroupList.jsx
@@ -12,64 +12,34 @@ export class _GroupList extends React.Component {
 
 
   // TASK DRAG
-  onDragEnd = (result) => {
-    const { destination, source, draggableId } = result;
-    const { group, board, updateBoard } = this.props;
-    if (!destination) return;
-    // console.log('destination:', destination);
-    // console.log('source:', source);
-    // console.log('draggableId:', draggableId);
 
-    const groupSourceIdx = board.groups.findIndex(
-      (group) => group.id === source.droppableId
-    );
-    // console.log('groupSourceIdx:', groupSourceIdx);
-
-    const task = board.groups[groupSourceIdx].tasks.find(
-      (task) => task.id === draggableId
-    );
-    const groupDestinationIdx = board.groups.findIndex(
-      (group) => group.id === destination.droppableId
-    );
-
-    // console.log('groupDestinationIdx:', groupDestinationIdx);
-
-    board.groups[groupSourceIdx].tasks.splice(source.index, 1);
-    board.groups[groupDestinationIdx].tasks.splice(destination.index, 0, task);
-    updateBoard(board);
-
-  };
 
   render() {
     const { board } = this.props;
     const { groups } = board;
     return (
 
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <section className="group-list"  >
+        {groups && groups.map((group, idx) => {
+          return (
+            <Draggable draggableId={group.id} index={idx} key={group.id}>
+              {({ draggableProps, dragHandleProps, placeholder, innerRef }) => (
 
-        <section className="group-list"  >
+                <div
+                  className="group-draggable-wrapper"
+                  {...draggableProps}
+                  {...dragHandleProps}
+                  ref={innerRef}
+                >
+                  <GroupPreview group={group} key={idx} idx={idx} board={board} />
+                  {placeholder}
+                </div>
+              )}
+            </Draggable>
 
-
-
-          {groups && groups.map((group, idx) => {
-            // console.log('group: ', group)
-            return (
-
-
-              <Droppable droppableId={group.id} key={idx}>
-                {(provided) => (
-                  <section ref={provided.innerRef} {...provided.droppableProps}>
-                    <GroupPreview group={group} key={idx} idx={idx} board={board} />
-                    {provided.placeholder}
-
-                  </section>
-                )}
-              </Droppable>
-            );
-          })}
-        </section>
-      </DragDropContext>
-
+          );
+        })}
+      </section>
     )
   }
 }
--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Droppable } from "react-beautiful-dnd";
 import { MainNav } from "../cmps/NavCmps/MainNav.jsx";
 import { loadBoard } from "../store/board.action.js";
 import { BoardHeader } from "../cmps/BoardCmps/BoardHeader.jsx";
 import { BoardNav } from "../cmps/NavCmps/BoardNav.jsx";
 import { GroupList } from "../cmps/GroupCmps/GroupList.jsx";
-import { Draggable } from "react-beautiful-dnd";
-import { DragDropContext } from "react-beautiful-dnd";
+import { Draggable, Droppable, DragDropContext } from "react-beautiful-dnd";
 
 import { setActiveModal, updateBoard } from "../store/board.action.js";
 
@@ -36,17 +34,41 @@ class _BoardDetails extends React.Component {
     });
   }
 
-  onDragEnd = (result) => {
-
-    const { destination, source, draggableId } = result;
-    const { group, board, updateBoard } = this.props;
-    console.log('destination:', destination);
-    console.log('source:', source);
-    console.log('draggableId:', draggableId);
-
- 
-
+  onDragEnd = ({ type, ...result }) => {
+    if (!result.destination) return;
+    if (type === 'board') {
+      this._onGroupDragEnd(result);
+    } else if (type === 'group') {
+      this._onTaskDragEnd(result);
+    }
   };
+
+  _onGroupDragEnd({ source, destination }) {
+    const { board, updateBoard } = this.props;
+    const boardCopy = { ...board };
+    const [group] = boardCopy.groups.splice(source.index, 1);
+    boardCopy.groups.splice(destination.index, 0, group);
+    updateBoard(boardCopy);
+  }
+
+  _onTaskDragEnd({ source, destination, draggableId }) {
+    const { board, updateBoard } = this.props;
+    const boardCopy = { ...board };
+    const groupSourceIdx = boardCopy.groups.findIndex(
+      (group) => group.id === source.droppableId
+    );
+
+    const task = boardCopy.groups[groupSourceIdx].tasks.find(
+      (task) => task.id === draggableId
+    );
+    const groupDestinationIdx = boardCopy.groups.findIndex(
+      (group) => group.id === destination.droppableId
+    );
+
+    boardCopy.groups[groupSourceIdx].tasks.splice(source.index, 1);
+    boardCopy.groups[groupDestinationIdx].tasks.splice(destination.index, 0, task);
+    updateBoard(boardCopy);
+  }
 
   render() {
     const { board, updateBoard, isBoardNavOpen } = this.props;
@@ -62,11 +84,29 @@ class _BoardDetails extends React.Component {
         <section className={boardContainerClassName}>
           <BoardHeader board={board} updateBoard={updateBoard} />
 
-          {board?.groups && (
-                    <GroupList board={board} />   
-         )} 
-     </section> 
-      </section> 
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            {board?.groups && (
+              <Droppable
+                droppableId={board._id}
+                type='board'
+              >
+                {(provided) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="group-list-wrapper"
+                  >
+                    <GroupList
+                      board={board}
+                    />
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            )}
+          </DragDropContext>
+        </section>
+      </section >
     );
   }
 }
