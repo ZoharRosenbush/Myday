32935cdcdd370355b83c691f96a68130c5aef5e6
ZoharRo <zozozohar@gmail.com>
2022-01-31 18:12:59 +0200

    lee

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -39,6 +39,7 @@ async function getById(boardId) {
 }
 
 async function saveBoard(boardToSave) {
+  console.log('the board in front',boardToSave)
   if (boardToSave._id) {
     const savedBoard = await httpService.put(`board/${boardToSave._id}`, boardToSave);
     socketService.emit('member updated board', boardToSave._id)
